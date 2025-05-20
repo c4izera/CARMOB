8c6b727df359bcdd40ad2039d083470707c501ad
caio placido <caioplacido02@gmail.com>
2025-05-20 08:52:44 -0300

    TextInput basic examples

--- ETAPA 2/app-react-exemplos/App2.js
+++ ETAPA 2/app-react-exemplos/App2.js
@@ -3,17 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
 
 import List from './components/List';
-import Inputs from './components/Inputs';
 
 export default function App() {
-
+ 
   return (
     <View style={styles.container}>
       <List />
       <View style={styles.redbox}></View>
       <View style={styles.bluebox}></View>
       <View style={styles.blackbox}></View>
-      <Inputs />
     </View>
   );
 }
