a0fcf697ed8b803d6de3a30ab06a0631644cf4a1
caio placido <caioplacido02@gmail.com>
2025-05-20 08:56:01 -0300

    ScrollView example

--- ETAPA 2/app-react-exemplos/App3.js
+++ ETAPA 2/app-react-exemplos/App3.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
 import Inputs from './components/Inputs';
-import ScrollViewExample from './components/ScrollViewExamples';
 
 export default function App() {
-
+ 
   return (
     <View style={styles.container}>
       <Inputs />
-      <ScrollViewExample />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 600,
+    marginTop: 150,
+  }
+});
