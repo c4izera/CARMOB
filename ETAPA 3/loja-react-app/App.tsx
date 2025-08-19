b0e7361ab67f7a041ca248c8e0811b71ecb5f387
caio placido <caioplacido02@gmail.com>
2025-08-19 07:58:48 -0300

    Navegação básica entre telas

--- ETAPA 3/loja-react-app/App.tsx
+++ ETAPA 3/loja-react-app/App.tsx
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+// filepath: /c:/Users/cg3027309/Documents/CARMOB/ETAPA 3/loja-react-app/App.tsx
 import { StyleSheet, Text, View } from 'react-native';
 
 import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
 import HomeScreen from './src/screens/HomeScreen';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <View style={{ flex: 1 }}>
-        <HomeScreen />
-      </View>
+      <HomeScreen />
+      <RootNavigator />
     </ThemeProvider>
   );
 }
