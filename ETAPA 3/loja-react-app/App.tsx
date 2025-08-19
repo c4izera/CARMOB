8f67d6be0fbb6c981d338cae3b6a8bd0361c56ad
caio placido <caioplacido02@gmail.com>
2025-08-19 07:21:25 -0300

    Incluir ThemeContext

--- ETAPA 3/loja-react-app/App.tsx
+++ ETAPA 3/loja-react-app/App.tsx
@@ -1,19 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { ThemeProvider } from './src/contexts/ThemeContext'; // NOVA
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-   <HomeScreen />
+    <ThemeProvider>
+      <View style={{ flex: 1 }}>
+        <HomeScreen />
+      </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
